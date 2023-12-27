16a7e34ff3f9d80cd04c3f012a2c56cb653b28f4
Your Name <you@example.com>
2023-12-27 02:06:20 +0000

    payload.bin

---
+++ dreamhack/1_stack_overflow_return_addr_overwrite/rao.c
@@ -0,0 +1,18 @@
+// Name: rao.c
+// Compile: gcc -o rao rao.c -fno-stack-protector -no-pie
+
+#include <stdio.h>
+
+void hidden_function() {
+  printf("Hello, Im Hidden!\n");
+}
+
+int main() {
+  char buf[0x28];
+
+  printf("Input: ");
+  scanf("%s", buf);
+  printf("your input : %s\n",buf);
+
+  return 0;
+}
